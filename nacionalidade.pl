e3643439fc4dc0ec2ee5dc2e85e8274c2587a07e
Raphael Paiva <raphaeldpaiva@gmail.com>
2011-06-28 10:47:39 -0300

    implementando o segundo caso

--- nacionalidade.pl
+++ nacionalidade.pl
@@ -6,16 +6,24 @@
 %%        Vinicius Bastos Bittencourt
 
 %% Fatos
-%% Caso 1:
 nasceu(mary, eua).
 nasceu(john, eua).
 nasceu(joao, brasil).
 
+nasceu(katja, alemanha).
+nasceu(maria, alemanha).
+
 mae(mary, joao).
 pai(john, joao).
 
+mae(katja, maria).
+pai(joao, maria).
+
 trabalha(mary, alemanha).
 trabalha(john, estonia).
+
+trabalha(joao, estonia).
+trabalha(katja, brasil).
 
 %% Cláusulas
 
@@ -24,9 +32,21 @@ trabalha(john, estonia).
 %% Pais sao estrangeiros.
 %% Pais nao trabalham a servico de seus paises de origem.
 
-nacionalidade(X) :- nasceu(X, brasil), %% Nasceu no brasil
-                    pai(Pai, X), mae(Mae, X), %% Pai e mãe são estrangeiros
-                    nasceu(Pai, PaisPai), nasceu(Mae, PaisMae),
+nacionalidade(X) :- nasceu(X, brasil),                                        %% Nasceu no brasil
+                    pai(Pai, X), mae(Mae, X),                                 %% Pai e mãe
+                    nasceu(Pai, PaisPai), nasceu(Mae, PaisMae),               %% são estrangeiros
                     not(PaisPai=brasil), not(PaisMae=brasil),
                     not(trabalha(Pai, PaisPai)), not(trabalha(Mae, PaisMae)). %% Pais não trabalham para seus países de origem.
-                   
+
+%% Caso 2:
+%% Pessoa nasceu no exterior.
+%% Pai ou mae sao Brasileiros.
+%% Pai ou mae trabalham a servico do Brasil.
+
+nacionalidade(X) :- not(nasceu(X, brasil)),
+                    pai(Pai, X), nasceu(Pai, PaisPai),
+                    mae(Mae, X), nasceu(Mae, PaisMae),
+                    trabalha(Pai, TPaisPai), trabalha(Mae, TPaisMae),
+                    (PaisPai=brasil; PaisMae=brasil),
+                    (TPaisPai=brasil; TPaisMae=brasil).
+                    
