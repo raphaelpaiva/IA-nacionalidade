6d2a384c69d4eddee7e4635640ecd5a11b86d931
Raphael Paiva <raphaeldpaiva@gmail.com>
2011-06-28 10:34:25 -0300

    rebuscando o primeiro caso

--- nacionalidade.pl
+++ nacionalidade.pl
@@ -6,9 +6,27 @@
 %%        Vinicius Bastos Bittencourt
 
 %% Fatos
-
+%% Caso 1:
+nasceu(mary, eua).
+nasceu(john, eua).
 nasceu(joao, brasil).
+
+mae(mary, joao).
+pai(john, joao).
+
+trabalha(mary, alemanha).
+trabalha(john, estonia).
 
 %% Cláusulas
 
-nacionalidade(X) :- nasceu(X, brasil).
+%% Caso 1:
+%% Pessoa nasceu no Brasil.
+%% Pais sao estrangeiros.
+%% Pais nao trabalham a servico de seus paises de origem.
+
+nacionalidade(X) :- nasceu(X, brasil), %% Nasceu no brasil
+                    pai(Pai, X), mae(Mae, X), %% Pai e mãe são estrangeiros
+                    nasceu(Pai, PaisPai), nasceu(Mae, PaisMae),
+                    not(PaisPai=brasil), not(PaisMae=brasil),
+                    not(trabalha(Pai, PaisPai)), not(trabalha(Mae, PaisMae)). %% Pais não trabalham para seus países de origem.
+                   
