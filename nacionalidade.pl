59296b4e57e9badb928dc2f3838976cf21da9363
Raphael Paiva <raphaeldpaiva@gmail.com>
2011-06-28 10:59:39 -0300

    implementando o terceiro caso

--- nacionalidade.pl
+++ nacionalidade.pl
@@ -13,11 +13,17 @@ nasceu(joao, brasil).
 nasceu(katja, alemanha).
 nasceu(maria, alemanha).
 
+nasceu(ramon, mexico).
+nasceu(monica, brasil).
+nasceu(florinda, mexico).
+
 mae(mary, joao).
 mae(katja, maria).
+mae(monica, florinda).
 
 pai(john, joao).
 pai(joao, maria).
+pai(ramon, florinda).
 
 trabalha(mary, alemanha).
 trabalha(john, estonia).
@@ -25,9 +31,12 @@ trabalha(john, estonia).
 trabalha(joao, estonia).
 trabalha(katja, brasil).
 
+registro(florinda, brasil).
+
 %% Cláusulas
 
 %% Caso 1:
+
 %% Pessoa nasceu no Brasil.
 %% Pais sao estrangeiros.
 %% Pais nao trabalham a servico de seus paises de origem.
@@ -39,6 +48,7 @@ nacionalidade(X) :- nasceu(X, brasil),                                        %%
                     not(trabalha(Pai, PaisPai)), not(trabalha(Mae, PaisMae)). %% Pais não trabalham para seus países de origem.
 
 %% Caso 2:
+
 %% Pessoa nasceu no exterior.
 %% Pai ou mae sao Brasileiros.
 %% Pai ou mae trabalham a servico do Brasil.
@@ -50,3 +60,14 @@ nacionalidade(X) :- not(nasceu(X, brasil)),
                     (PaisPai=brasil; PaisMae=brasil),
                     (TPaisPai=brasil; TPaisMae=brasil).
 
+%% Caso 3:
+
+%% Pessoa nasceu no exterior.
+%% Pai ou mae da pessoa eh brasileiro(a).
+%% Pessoa registrada em reparticao brasileira.
+
+nacionalidade(X) :- not(nasceu(X, brasil)),
+                    pai(Pai, X), nasceu(Pai, PaisPai),
+                    mae(Mae, X), nasceu(Mae, PaisMae),
+                    (PaisPai=brasil; PaisMae=brasil),
+                    registro(X, brasil).
